e2cc3d1a2c27ce2bb8e0a3381fbad1cfedb9f867
Patrizio Gelosi <patrizio.gelosi@amarulasolutions.com>
2026-07-09 11:02:05 +0200

    feat: report unavailable dev features
    Show a specific message when a dev-only command is attempted in a
    production build.
    This avoids generic failures and clarifies why the command is
    unavailable.
    
    Change-Id: I8582bcc9930637d066b36eaeff85633db7ed2675
    Signed-off-by: Patrizio Gelosi <patrizio.gelosi@amarulasolutions.com>

---
+++ src/test/java/com/googlesource/gerrit/plugins/reviewai/aibackend/common/client/commands/ClientCommandParserTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2026. The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.googlesource.gerrit.plugins.reviewai.aibackend.common.client.commands;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.googlesource.gerrit.plugins.reviewai.aibackend.common.client.api.gerrit.GerritChange;
+import com.googlesource.gerrit.plugins.reviewai.aibackend.common.model.data.ChangeSetData;
+import com.googlesource.gerrit.plugins.reviewai.config.Configuration;
+import com.googlesource.gerrit.plugins.reviewai.localization.Localizer;
+import com.googlesource.gerrit.plugins.reviewai.localization.SystemMessageFormatter;
+import java.util.List;
+import org.junit.Test;
+
+public class ClientCommandParserTest {
+  @Test
+  public void productionBuildRejectsDevOnlyCommandsWithDevBuildRequiredMessage() {
+    for (String command : List.of("/show", "/review --debug", "/configure", "/directives")) {
+      ChangeSetData changeSetData = new ChangeSetData(1);
+      Localizer localizer = localizer();
+      ClientCommandParser parser =
+          new ClientCommandParser(
+              mock(Configuration.class),
+              changeSetData,
+              mock(GerritChange.class),
+              null,
+              null,
+              localizer,
+              null,
+              null);
+
+      assertTrue(parser.parseCommands(command));
+
+      assertEquals(
+          SystemMessageFormatter.getLocalizedWarningMessage(
+              localizer, "message.command.dev.build.required"),
+          changeSetData.getReviewSystemMessage());
+    }
+  }
+
+  private static Localizer localizer() {
+    Localizer localizer = mock(Localizer.class);
+    when(localizer.getText("plugin.message.prefix")).thenReturn("ReviewAI");
+    when(localizer.getText("plugin.message.label")).thenReturn("Message");
+    when(localizer.getText("plugin.warning.label")).thenReturn("**WARNING**");
+    when(localizer.getText("plugin.error.label")).thenReturn("**ERROR**");
+    when(localizer.getText("message.command.dev.build.required"))
+        .thenReturn("Unable to execute command: the -dev build is required");
+    return localizer;
+  }
+}
