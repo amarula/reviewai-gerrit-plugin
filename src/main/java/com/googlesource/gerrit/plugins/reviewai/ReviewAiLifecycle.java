52d78a486616cf3026a8efb87df10c63c4eee1c0
Patrizio Gelosi <patrizio.gelosi@amarulasolutions.com>
2026-07-16 15:50:29 +0200

    chore: clean up shutdown resources
    Close database connections and remove unused worker threads when the
    plugin shuts down.
    
    This improves plugin cleanup and avoids leaving resources active after
    shutdown.
    
    Change-Id: I306933e3310685669d8f61680f3596a4bd941666
    Suggested-by: Michael Trimarchi <michael@amarulasolutions.com>
    Signed-off-by: Patrizio Gelosi <patrizio.gelosi@amarulasolutions.com>

---
+++ src/main/java/com/googlesource/gerrit/plugins/reviewai/ReviewAiLifecycle.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2026. The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.googlesource.gerrit.plugins.reviewai;
+
+import com.google.gerrit.extensions.events.LifecycleListener;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import com.googlesource.gerrit.plugins.reviewai.data.ReviewAiDb;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Singleton
+public class ReviewAiLifecycle implements LifecycleListener {
+  private final ReviewAiExecutors reviewAiExecutors;
+
+  @Inject
+  ReviewAiLifecycle(ReviewAiExecutors reviewAiExecutors) {
+    this.reviewAiExecutors = reviewAiExecutors;
+  }
+
+  @Override
+  public void start() {
+    log.info("Starting ReviewAI lifecycle");
+  }
+
+  @Override
+  public void stop() {
+    log.info("Stopping ReviewAI lifecycle");
+    reviewAiExecutors.shutdown();
+    ReviewAiDb.stopManagedTcpServer();
+  }
+}
