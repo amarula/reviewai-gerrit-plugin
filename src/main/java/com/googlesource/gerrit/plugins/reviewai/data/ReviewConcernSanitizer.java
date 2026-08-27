amarula/reviewai-gerrit-plugin

commit 646b6d2ad41324469da2eed8cb6fb36e68cc4a7b

feat(concerns): sanitize stale concern ledgers at startup
This just pick the changes that are merged that was not receive the notification
event of abandon or merged. This allow to cleanup context even was a problem
during the gerrit execution

Change-Id: Ia3aa078f199db9410c63b649b94c7b0f9a051042
Signed-off-by: Michael Trimarchi <michael@amarulasolutions.com>

src/main/java/com/googlesource/gerrit/plugins/reviewai/data/ReviewConcernSanitizer.java
@@ -1,0 +1,95 @@
+/*
+ * Copyright (c) 2026. Amarula Solutions
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.googlesource.gerrit.plugins.reviewai.data;
+
+import com.google.gerrit.extensions.api.GerritApi;
+import com.google.gerrit.extensions.client.ChangeStatus;
+import com.google.gerrit.extensions.common.ChangeInfo;
+import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
+import com.google.gerrit.server.util.ManualRequestContext;
+import com.google.gerrit.server.util.OneOffRequestContext;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import java.sql.SQLException;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+
+/** Removes concern ledgers for changes that are no longer open (merged, abandoned, or gone). */
+@Slf4j
+@Singleton
+public class ReviewConcernSanitizer {
+  private final ReviewAiDb db;
+  private final GerritApi gerritApi;
+  private final OneOffRequestContext requestContext;
+
+  @Inject
+  public ReviewConcernSanitizer(
+      ReviewAiDb db, GerritApi gerritApi, OneOffRequestContext requestContext) {
+    this.db = db;
+    this.gerritApi = gerritApi;
+    this.requestContext = requestContext;
+  }
+
+  public int sanitize() {
+    List<String> changeIds;
+    try {
+      changeIds = db.listReviewConcernChangeIds();
+    } catch (SQLException e) {
+      log.error("Could not list review concern change IDs for sanitization", e);
+      return 0;
+    }
+    if (changeIds.isEmpty()) {
+      return 0;
+    }
+
+    int removed = 0;
+    try (ManualRequestContext ignored = requestContext.open()) {
+      for (String changeId : changeIds) {
+        if (removeIfClosed(changeId)) {
+          removed++;
+        }
+      }
+    }
+    log.info("Review concern sanitization removed {} of {} ledgers", removed, changeIds.size());
+    return removed;
+  }
+
+  private boolean removeIfClosed(String changeId) {
+    int separator = changeId.lastIndexOf('~');
+    if (separator <= 0 || separator == changeId.length() - 1) {
+      log.warn("Skipping review concern ledger with malformed change id: {}", changeId);
+      return false;
+    }
+    String changeKey = changeId.substring(separator + 1);
+    try {
+      ChangeInfo info = gerritApi.changes().id(changeKey).get();
+      if (info.status == ChangeStatus.MERGED || info.status == ChangeStatus.ABANDONED) {
+        new ReviewConcernStore(db, changeId).clear();
+        log.info("Removed review concern ledger for {} change {}", info.status, changeId);
+        return true;
+      }
+    } catch (ResourceNotFoundException e) {
+      new ReviewConcernStore(db, changeId).clear();
+      log.info("Removed review concern ledger for missing change {}", changeId);
+      return true;
+    } catch (Exception e) {
+      log.warn(
+          "Could not resolve status of change {}; keeping its review concern ledger", changeId, e);
+    }
+    return false;
+  }
+}

src/test/java/com/googlesource/gerrit/plugins/reviewai/data/ReviewConcernSanitizerTest.java
@@ -1,0 +1,82 @@
+/*
+ * Copyright (c) 2026. Amarula Solutions
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.googlesource.gerrit.plugins.reviewai.data;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.google.gerrit.extensions.api.GerritApi;
+import com.google.gerrit.extensions.api.changes.ChangeApi;
+import com.google.gerrit.extensions.api.changes.Changes;
+import com.google.gerrit.extensions.client.ChangeStatus;
+import com.google.gerrit.extensions.common.ChangeInfo;
+import com.google.gerrit.extensions.restapi.ResourceNotFoundException;
+import com.google.gerrit.server.util.OneOffRequestContext;
+import com.googlesource.gerrit.plugins.reviewai.TestBase;
+import com.googlesource.gerrit.plugins.reviewai.aibackend.common.model.review.ReviewConcernLedger;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ReviewConcernSanitizerTest extends TestBase {
+  @Mock private GerritApi gerritApi;
+  @Mock private Changes changes;
+  @Mock private OneOffRequestContext requestContext;
+
+  private ReviewConcernSanitizer sanitizer;
+  private ReviewAiDb db;
+
+  @Before
+  public void setUp() {
+    db = getTestReviewAiDb();
+    sanitizer = new ReviewConcernSanitizer(db, gerritApi, requestContext);
+  }
+
+  @Test
+  public void removesMergedAbandonedAndMissingLedgersKeepsOpenOnes() throws Exception {
+    new ReviewConcernStore(db, "p~main~Imerged").save(new ReviewConcernLedger());
+    new ReviewConcernStore(db, "p~main~Iabandoned").save(new ReviewConcernLedger());
+    new ReviewConcernStore(db, "p~main~Inew").save(new ReviewConcernLedger());
+    new ReviewConcernStore(db, "p~main~Imissing").save(new ReviewConcernLedger());
+
+    ChangeApi mergedApi = changeApiWithStatus(ChangeStatus.MERGED);
+    ChangeApi abandonedApi = changeApiWithStatus(ChangeStatus.ABANDONED);
+    ChangeApi newApi = changeApiWithStatus(ChangeStatus.NEW);
+
+    when(gerritApi.changes()).thenReturn(changes);
+    when(changes.id("Imerged")).thenReturn(mergedApi);
+    when(changes.id("Iabandoned")).thenReturn(abandonedApi);
+    when(changes.id("Inew")).thenReturn(newApi);
+    when(changes.id("Imissing")).thenThrow(new ResourceNotFoundException("gone"));
+
+    assertEquals(3, sanitizer.sanitize());
+    assertEquals(List.of("p~main~Inew"), db.listReviewConcernChangeIds());
+  }
+
+  private ChangeApi changeApiWithStatus(ChangeStatus status) throws Exception {
+    ChangeApi api = mock(ChangeApi.class);
+    ChangeInfo info = new ChangeInfo();
+    info.status = status;
+    when(api.get()).thenReturn(info);
+    return api;
+  }
+}

src/main/java/com/googlesource/gerrit/plugins/reviewai/ReviewAiLifecycle.java
@@ -26,6 +26,7 @@ import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.google.inject.Singleton;
 import com.googlesource.gerrit.plugins.reviewai.data.ReviewAiDb;
+import com.googlesource.gerrit.plugins.reviewai.data.ReviewConcernSanitizer;
 import com.googlesource.gerrit.plugins.reviewai.listener.GerritListener;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
@@ -48,6 +49,7 @@ public class ReviewAiLifecycle implements LifecycleListener {
   private final Provider<GerritListener> gerritListenerProvider;
   private final DynamicSet<EventListener> eventListeners;
   private final ReviewAiDb reviewAiDb;
+  private final ReviewConcernSanitizer reviewConcernSanitizer;
   private final String pluginName;
   private final String listenerClassName;
   private RegistrationHandle listenerHandle;
@@ -58,12 +60,14 @@ public class ReviewAiLifecycle implements LifecycleListener {
       Provider<GerritListener> gerritListenerProvider,
       DynamicSet<EventListener> eventListeners,
       ReviewAiDb reviewAiDb,
+      ReviewConcernSanitizer reviewConcernSanitizer,
       @PluginName String pluginName) {
     this(
         reviewAiExecutors,
         gerritListenerProvider,
         eventListeners,
         reviewAiDb,
+        reviewConcernSanitizer,
         pluginName,
         GerritListener.class.getName());
   }
@@ -73,12 +77,14 @@ public class ReviewAiLifecycle implements LifecycleListener {
       Provider<GerritListener> gerritListenerProvider,
       DynamicSet<EventListener> eventListeners,
       ReviewAiDb reviewAiDb,
+      ReviewConcernSanitizer reviewConcernSanitizer,
       String pluginName,
       String listenerClassName) {
     this.reviewAiExecutors = reviewAiExecutors;
     this.gerritListenerProvider = gerritListenerProvider;
     this.eventListeners = eventListeners;
     this.reviewAiDb = reviewAiDb;
+    this.reviewConcernSanitizer = reviewConcernSanitizer;
     this.pluginName = pluginName;
     this.listenerClassName = listenerClassName;
   }
@@ -101,6 +107,17 @@ public class ReviewAiLifecycle implements LifecycleListener {
     GerritListener listener = gerritListenerProvider.get();
     listenerHandle = eventListeners.add(pluginName, listener);
     log.info("Registered GerritListener for plugin '{}'", pluginName);
+
+    reviewAiExecutors
+        .getAgentExecutor()
+        .execute(
+            () -> {
+              try {
+                reviewConcernSanitizer.sanitize();
+              } catch (Exception e) {
+                log.error("Review concern sanitization failed", e);
+              }
+            });
   }
 
   @Override

src/main/java/com/googlesource/gerrit/plugins/reviewai/data/ReviewAiDb.java
@@ -28,10 +28,13 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Properties;
 import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
@@ -191,6 +194,19 @@ public class ReviewAiDb {
             + ")",
         "CREATE INDEX IF NOT EXISTS idx_review_concerns_change_status"
             + " ON review_concerns(change_id, concern_status)");
+  }
+
+  public List<String> listReviewConcernChangeIds() throws SQLException {
+    try (Connection connection = getConnection();
+        PreparedStatement statement =
+            connection.prepareStatement("SELECT change_id FROM review_concern_ledgers");
+        ResultSet results = statement.executeQuery()) {
+      List<String> changeIds = new ArrayList<>();
+      while (results.next()) {
+        changeIds.add(results.getString(1));
+      }
+      return changeIds;
+    }
   }
 
   public void initReviewFeedbackSchema() throws SQLException {

src/test/java/com/googlesource/gerrit/plugins/reviewai/ReviewAiLifecycleTest.java
@@ -20,6 +20,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import com.google.gerrit.extensions.registration.DynamicSet;
 import com.google.gerrit.extensions.registration.Extension;
@@ -28,6 +29,7 @@ import com.google.gerrit.server.events.Event;
 import com.google.gerrit.server.events.EventListener;
 import com.google.inject.Provider;
 import com.googlesource.gerrit.plugins.reviewai.data.ReviewAiDb;
+import com.googlesource.gerrit.plugins.reviewai.data.ReviewConcernSanitizer;
 import com.googlesource.gerrit.plugins.reviewai.listener.GerritListener;
 import org.junit.Test;
 
@@ -217,11 +219,13 @@ public class ReviewAiLifecycleTest {
       DynamicSet<EventListener> eventListeners,
       ReviewAiExecutors executors,
       ReviewAiDb reviewAiDb) {
+    when(executors.getAgentExecutor()).thenReturn(command -> command.run());
     return new ReviewAiLifecycle(
         executors,
         MOCK_LISTENER_PROVIDER,
         eventListeners,
         reviewAiDb,
+        mock(ReviewConcernSanitizer.class),
         PLUGIN_NAME,
         TargetEventListener.class.getName());
   }

commit fe169dab123987ca49a466f6e0b3d277b5ebf0f5

fix(concerns): resolve ledgers with full change IDs
Pass the stored project, branch, and Change-Id triplet to Gerrit when
checking ledger status. A bare Change-Id can match multiple changes, and
Gerrit reports that ambiguity as ResourceNotFoundException, which caused
the sanitizer to remove an active concern ledger.

Fixes: 646b6d2a (feat(concerns): sanitize stale concern ledgers at startup)

AI-assisted-by: OpenAI Codex:gpt-5.6-sol
Change-Id: I7dea6eb228e26bdf809dd02e941f3d20913fa799
Signed-off-by: Patrizio Gelosi <patrizio.gelosi@amarulasolutions.com>

src/main/java/com/googlesource/gerrit/plugins/reviewai/data/ReviewConcernSanitizer.java
@@ -82,11 +82,9 @@ public class ReviewConcernSanitizer {
       log.warn("Skipping review concern ledger with malformed change id: {}", changeId);
       return false;
     }
-    String changeKey = changeId.substring(separator + 1);
-
     boolean shouldClear;
     try {
-      ChangeInfo info = gerritApi.changes().id(changeKey).get();
+      ChangeInfo info = gerritApi.changes().id(changeId).get();
       shouldClear = info.status == ChangeStatus.MERGED || info.status == ChangeStatus.ABANDONED;
     } catch (ResourceNotFoundException e) {
       shouldClear = true;

src/test/java/com/googlesource/gerrit/plugins/reviewai/data/ReviewConcernSanitizerTest.java
@@ -52,7 +52,8 @@ public class ReviewConcernSanitizerTest extends TestBase {
   }
 
   @Test
-  public void removesMergedAbandonedAndMissingLedgersKeepsOpenOnes() throws Exception {
+  public void removesMergedAbandonedAndMissingLedgersKeepsOpenOnesUsingFullChangeIds()
+      throws Exception {
     new ReviewConcernStore(db, "p~main~Imerged").save(new ReviewConcernLedger());
     new ReviewConcernStore(db, "p~main~Iabandoned").save(new ReviewConcernLedger());
     new ReviewConcernStore(db, "p~main~Inew").save(new ReviewConcernLedger());
@@ -63,10 +64,10 @@ public class ReviewConcernSanitizerTest extends TestBase {
     ChangeApi newApi = changeApiWithStatus(ChangeStatus.NEW);
 
     when(gerritApi.changes()).thenReturn(changes);
-    when(changes.id("Imerged")).thenReturn(mergedApi);
-    when(changes.id("Iabandoned")).thenReturn(abandonedApi);
-    when(changes.id("Inew")).thenReturn(newApi);
-    when(changes.id("Imissing")).thenThrow(new ResourceNotFoundException("gone"));
+    when(changes.id("p~main~Imerged")).thenReturn(mergedApi);
+    when(changes.id("p~main~Iabandoned")).thenReturn(abandonedApi);
+    when(changes.id("p~main~Inew")).thenReturn(newApi);
+    when(changes.id("p~main~Imissing")).thenThrow(new ResourceNotFoundException("gone"));
 
     assertEquals(3, sanitizer.sanitize());
     assertEquals(List.of("p~main~Inew"), db.listReviewConcernChangeIds());